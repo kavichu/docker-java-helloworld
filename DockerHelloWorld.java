6a28bb877e72e82d932840cc848855d660880d75
Luis Valdes <luisvaldes88@gmail.com>
2016-03-06 22:50:16 -0300

    Playing with Docker Compose

--- DockerHelloWorld.java
+++ DockerHelloWorld.java
@@ -1,5 +1,9 @@
+import redis.clients.jedis.Jedis;
+
 public class DockerHelloWorld {
   public static void main(String[] args) {
+    Jedis jedis = new Jedis("redis");
     System.out.println("Hello world from Docker!");
+    System.out.println("Server is running: " + jedis.ping());
   }
 }
