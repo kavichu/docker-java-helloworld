b4b11c6c434e97d1ff1a2a9d8e60e2d4040940e6
Luis Valdes <luischivaldes@gmail.com>
2016-07-20 08:44:45 -0300

    We need to build!

--- DockerHelloWorld.java
+++ DockerHelloWorld.java
@@ -5,5 +5,6 @@ public class DockerHelloWorld {
     Jedis jedis = new Jedis("redis");
     System.out.println("Hello world from Docker!");
     System.out.println("Server is running: " + jedis.ping());
+    System.out.println("Goodbye from Docker!");
   }
 }
